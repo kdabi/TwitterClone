19e2839e244df176597d30321e257d355bac2a33
kdabi <kritagya577@gmail.com>
2018-04-07 02:18:43 +0530

    added first javascript code

--- src/API/Server.hs
+++ src/API/Server.hs
@@ -1,12 +1,12 @@
 module API.Server where
 
 import Control.Monad (msum)
-import Happstack.Server (Method(GET, HEAD, POST), method, dir, nullDir, ok, nullConf, simpleHTTP, toResponse)
+import Control.Monad.State (evalStateT)
+import Happstack.Server (ServerPartT, Method(GET, HEAD, POST), method, dir, nullDir, ok, nullConf, simpleHTTP, toResponse, mapServerPartT)
 
-import HTML.Home (helloBlaze)
+import HTML.Home (helloBlaze, helloJMacro, handlers, JMacroPart)
 
-twitterClone = simpleHTTP nullConf $ msum [ dir "home" $ do method [GET, HEAD] >> nullDir
-                                                            helloBlaze
-                                          , do method GET
-                                               ok $ (toResponse "Page Doesn't Exists.\n")
-                                          ]
+twitterClone = simpleHTTP nullConf $ flatten handlers
+            where
+                flatten :: JMacroPart a -> ServerPartT IO a
+                flatten = mapServerPartT (flip evalStateT 0)
--- src/HTML/Home.hs
+++ src/HTML/Home.hs
@@ -1,11 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP, FlexibleInstances,  GeneralizedNewtypeDeriving, TypeSynonymInstances, QuasiQuotes#-}
+{-# OPTIONS_GHC -F -pgmF hsx2hs -fno-warn-orphans #-}
 module HTML.Home where
 
-import Control.Monad (forM_)
-
-import Happstack.Server
+import Control.Applicative ((<$>), optional)
+import Control.Monad       (msum, forM_)
+import Control.Monad.State (StateT, evalStateT)
+import Control.Monad.Trans (liftIO)
+import qualified Data.Map  as Map
+import Data.Maybe          (fromMaybe)
+import Happstack.Server    
+import Happstack.Server.HSP.HTML  (defaultTemplate) -- ^ also imports 'ToMessage XML'
+import Happstack.Server.JMacro    (jmResponse)      -- ^ ToMessage instance for JStat
+import HSP                        ( Attr(..), EmbedAsAttr(..), EmbedAsChild(..), genElement, genEElement, fromStringLit)
+import HSP.ServerPartT            () -- ^ instance 'XMLGenerator ServerPartT'
+import HSP.JMacro                 ( IntegerSupply(..), nextInteger') -- EmbedAsChild & EmbedAsAttr for JStat
+import Language.Javascript.JMacro ( ToJExpr(..), Ident(..), JStat(..), JExpr(..), JVal(..), jmacro, jsv, jLam, jVarTy)
+import System.Random              (Random(..))
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
+
+type JMacroPart = ServerPartT (StateT Integer IO)
+
+instance IntegerSupply JMacroPart where
+    nextInteger = nextInteger'
 
 appTemplate :: String -> [H.Html] -> H.Html -> H.Html
 appTemplate title headers body =
@@ -23,3 +40,18 @@ helloBlaze =
     appTemplate "TwitterClone" 
                  [H.meta ! A.name "keywords" ! A.content "happstack, blaze, html"] 
                  (H.p "Yo baby")
+
+helloJMacro :: JMacroPart Response
+helloJMacro =
+    toResponse <$> defaultTemplate "Hello JMacro" ()
+      <div>
+       <% [$jmacro|
+           var helloNode = document.createElement('h1');
+           helloNode.appendChild(document.createTextNode("Hello, JMacro!"));
+           document.body.appendChild(helloNode);
+           |] %>
+      </div>
+
+handlers :: JMacroPart Response
+handlers = 
+    msum [Happstack.Server.dir "hello" $ helloJMacro]
